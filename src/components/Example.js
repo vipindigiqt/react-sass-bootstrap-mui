f9ade0dee41f695b452e5994be1bb06aa0f49aaf
Vipin Mishra <vipin@digiqt.com>
2022-01-16 19:31:28 +0530

    added react-router-dom

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,5 +1,10 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Invenotry2 from "@mui/icons-material/Inventory2";
 
 import Container from "./common/Container";
 import Counter from "./Counter";
@@ -15,17 +20,47 @@ const Example = () => {
           Available packages by default
         </h4>
 
-        <ul className="list-group">
-          <li className="list-group-item">Sass</li>
-          <li className="list-group-item">
-            Bootstrap 5 (Fully customizable using Sass + Initial Setup)
-          </li>
-          <li className="list-group-item">MUI 5 (Fully Customizable Setup)</li>
-          <li className="list-group-item">axios</li>
-          <li className="list-group-item">
-            Prettier, ESLint, Git Hooks Configured.
-          </li>
-        </ul>
+        <List sx={{ border: "1px solid #ddd" }}>
+          <ListItem>
+            <ListItemIcon>
+              <Invenotry2 />
+            </ListItemIcon>
+            <ListItemText primary="Sass" secondary="Dart Sass" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <Invenotry2 />
+            </ListItemIcon>
+            <ListItemText
+              primary="Bootstrap 5"
+              secondary="Fully customizable using Sass + Initial Setup"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <Invenotry2 />
+            </ListItemIcon>
+            <ListItemText
+              primary="MUI 5"
+              secondary="Fully Customizable Setup"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <Invenotry2 />
+            </ListItemIcon>
+            <ListItemText primary="axios" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <Invenotry2 />
+            </ListItemIcon>
+            <ListItemText
+              primary="Prettier, ESLint, Git Hooks Configured"
+              secondary="To avoid unwanted errors and performance boost."
+            />
+          </ListItem>
+        </List>
 
         <div className="mt-5 d-grid gap-1">
           <Button
@@ -39,11 +74,6 @@ const Example = () => {
             )
           </Button>
           <small className="text-secondary">Click to Toggle Variant</small>
-        </div>
-
-        <div className="mt-5 d-grid gap-1">
-          <button className="btn btn-primary">GitHub</button>
-          <small>A button from Bootstrap 5</small>
         </div>
 
         <div className="mt-5">
